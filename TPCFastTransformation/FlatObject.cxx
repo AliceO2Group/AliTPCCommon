2c05b101652d8cd418dcac8bf5eeb7f4ceb0615d
sgorbuno <sergey.gorbunov@fias.uni-frankfurt.de>
2018-08-27 14:03:50 +0200

    working version of fast TPC transformation with irregular splines and flat buffer

---
+++ TPCFastTransformation/FlatObject.cxx
@@ -0,0 +1,27 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+
+/// \file  FlatObject.cxx
+/// \brief Implementation of FlatObject class
+///
+/// \author  Sergey Gorbunov <sergey.gorbunov@cern.ch>
+
+
+#include "FlatObject.h"
+
+namespace ali_tpc_common {
+namespace tpc_fast_transformation {
+
+
+
+
+}// namespace
+}// namespace
